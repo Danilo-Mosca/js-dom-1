e46820021c36ed97b644347c05ff1b7a76763716
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-07 16:12:57 +0100

    add script.js istruction for first exercise complete

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
 "use strict";
 console.clear();
 
+// Seleziono il pulsante
+const btnTurn = document.getElementById("turnLight");
+// Verifico da console.log se ho selezionato correttamente il pulsante
+console.log(btnTurn);
+
+// Seleziono l'immagine
+const imageSelected = document.getElementById("imageLamp");
+// Verifico console.log se ho selezionato correttamente l'immagine
+console.log(imageSelected);
+
+// Creo l'ascoltatore di eventi
+btnTurn.addEventListener("click", function() {
+    imageSelected.src = "../img/yellow_lamp.png";
+    // L'istruzione seguente capovolge l'immagine orizzontalmente
+    // per permettere alle due immagini di essere visualizzate nello
+    // stesso punto (altrimenti l'immagine con la lampadina accesa)
+    // verrebbe visualizzata leggermente più a destra rispetto a quella
+    // spenta
+    imageSelected.style.transform = "scaleX(-1)";
+});
